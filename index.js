22209f00d85e4237d4d215930085ddbcf16a5fb8
andreyyarchuk <dron4432@gmail.com>
2023-12-05 14:12:09 +0200

    1.0.0 - creat server on http

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello word')
+const http = require('http')
+const port = 8080
+
+const server = http.createServer((req, res) => {
+    res.end('hello word')
+})
+
+server.listen(port, () => {
+    console.log('server is listening on: http://localhost:%s', port)
+})
