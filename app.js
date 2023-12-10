7eab0181c57964c599ae12415af9c1f38844756f
andreyyarchuk <dron4432@gmail.com>
2023-12-10 21:49:55 +0200

    3.0.1: tamplate API

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const articles = [{titles:'Example'}, {titles:'Example2'}, {titles:'Example3'}]
 
-const port = process.env.PORT || 3001
+app.set('port', process.env.PORT || 3001)
 
-app.get('/', (req, res) => {
-    res.send('Hello word')
+
+app.get('/articles', (req, res, next) => {
+    res.send(articles)
 })
 
-app.listen(port, () => {
-    console.log(`Web app available at http://127.0.0.1:${port}`)
+app.post('/articles', (req, res, next) => {
+    res.send('ok')
 })
+
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log("Fetching:", id)
+    res.send(articles[id])
+})
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log("Deleting:", id)
+    delete articles[id]
+    res.send({message: 'Deleted'})
+})
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Web app available at http://127.0.0.1:${app.get('port')}`)
+})
+
+module.exports = app
